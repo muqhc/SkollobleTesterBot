628c057fb1762cfe43bbf708d7b0ec11283f3a31
muqhc <88622655+muqhc@users.noreply.github.com>
2022-02-24 22:12:01 +0900

    fix links

--- main-bot/src/main/kotlin/io/github/muqhc/skblebot/util/Usage.kt
+++ main-bot/src/main/kotlin/io/github/muqhc/skblebot/util/Usage.kt
@@ -2,8 +2,8 @@ package io.github.muqhc.skblebot.util
 
 object Usage {
     val data = mapOf(
-        Regex("^(en|english|eng|en-US)$") to "Usage =>> https://discord.gg/yj9YKDyw",
-        Regex("^(ko|korean|kor)$") to "도움말 =>> https://discord.gg/VNzuY5KT"
+        Regex("^(en|english|eng|en-US)$") to "Usage =>> https://discord.gg/4dfegEQ3k5",
+        Regex("^(ko|korean|kor)$") to "도움말 =>> https://discord.gg/eBtQAASJCH"
     )
     operator fun get(key: String): String? = data.run {
         var result: String? = null
